7a143da1fd1b4f18f611da0b092c5415de9af417
roman <romanrr24@mail.ru>
2023-09-13 18:06:54 +0300

    Mock тесты (разбил на отдельные тесты)

--- src/test/java/ru/aston/intensive/intensive34/MockOrderTest.java
+++ src/test/java/ru/aston/intensive/intensive34/MockOrderTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import ru.aston.druzhinin_ri.task4.model.Order;
+import ru.aston.druzhinin_ri.task4.model.User;
 import ru.aston.druzhinin_ri.task4.repository.RepositoryData;
 import ru.aston.druzhinin_ri.task4.repository.RepositoryImplOrder;
 
@@ -11,24 +12,32 @@ import java.util.List;
 
 public class MockOrderTest {
 
+    private static RepositoryImplOrder orderImpl = Mockito.mock(RepositoryImplOrder.class);
+    private static RepositoryData data = Mockito.mock(RepositoryData.class);
+
     private static Order order = new Order(111);
 
     @Test
-    void usersDAOImlMockitoTest(){
-        RepositoryImplOrder orderImpl = Mockito.mock(RepositoryImplOrder.class);
-        RepositoryData data = Mockito.mock(RepositoryData.class);
-        Mockito.when(data.scriptRun()).thenReturn(true);
-        Mockito.when(orderImpl.findEntityById(1)).thenReturn(order);
+    void findAll() {
         Mockito.when(orderImpl.findAll()).thenReturn(List.of(new Order(1,111)));
-        Mockito.when(orderImpl.update(new Order(1, 222))).thenReturn(new Order());
-        Mockito.when(orderImpl.delete(1)).thenReturn(true);
+        Assertions.assertNotNull(orderImpl.findAll());
+    }
 
-        Assertions.assertAll(
-                () -> Assertions.assertTrue(data.scriptRun()),
-                () -> Assertions.assertInstanceOf(order.getClass(), orderImpl.findEntityById(1)),
-                () -> Assertions.assertNotNull(orderImpl.findAll()),
-                () -> Assertions.assertInstanceOf(Order.class, orderImpl.update(new Order(1, 222))),
-                () -> Assertions.assertTrue(orderImpl.delete(1))
-        );
+    @Test
+    void findEntityById() {
+        Mockito.when(orderImpl.findEntityById(1)).thenReturn(order);
+        Assertions.assertInstanceOf(order.getClass(), orderImpl.findEntityById(1));
+    }
+
+    @Test
+    void delete() {
+        Mockito.when(orderImpl.delete(1)).thenReturn(true);
+        Assertions.assertTrue(orderImpl.delete(1));
+    }
+
+    @Test
+    void update() {
+        Mockito.when(orderImpl.update(new Order(1, 222))).thenReturn(new Order());
+        Assertions.assertInstanceOf(Order.class, orderImpl.update(new Order(1, 222)));
     }
 }
--- src/test/java/ru/aston/intensive/intensive34/MockUserTest.java
+++ src/test/java/ru/aston/intensive/intensive34/MockUserTest.java
@@ -9,26 +9,35 @@ import ru.aston.druzhinin_ri.task4.repository.RepositoryImplUser;
 
 import java.util.List;
 
+
 public class MockUserTest {
 
+    private static RepositoryData data = Mockito.mock(RepositoryData.class);
+    private static RepositoryImplUser userImpl = Mockito.mock(RepositoryImplUser.class);
     private static User user = new User("Roman Dru", "+7(999)000-00-00" , "roman@mail.ru", 1);
 
     @Test
-    void usersDAOImlMockitoTest(){
-        RepositoryImplUser userImpl = Mockito.mock(RepositoryImplUser.class);
-        RepositoryData data = Mockito.mock(RepositoryData.class);
-        Mockito.when(data.scriptRun()).thenReturn(true);
-        Mockito.when(userImpl.findEntityById(1)).thenReturn(user);
+    void findAll() {
         Mockito.when(userImpl.findAll()).thenReturn(List.of(new User(1,"Roman Dru", "+7(999)000-00-00" , "roman@mail.ru", 1)));
-        Mockito.when(userImpl.update(new User(1,"Roman Druz", "+7(999)111-00-00" , "roman@mail.ru", 1))).thenReturn(new User());
-        Mockito.when(userImpl.delete(1)).thenReturn(true);
-
-        Assertions.assertAll(
-                () -> Assertions.assertTrue(data.scriptRun()),
-                () -> Assertions.assertInstanceOf(user.getClass(), userImpl.findEntityById(1)),
-                () -> Assertions.assertNotNull(userImpl.findAll()),
-                () -> Assertions.assertInstanceOf(User.class, userImpl.update(new User(1,"Roman Druz", "+7(999)111-00-00" , "roman@mail.ru", 1))),
-                () -> Assertions.assertTrue(userImpl.delete(1))
-        );
+        Assertions.assertNotNull(userImpl.findAll());
     }
+
+    @Test
+    void findEntityById() {
+        Mockito.when(userImpl.findEntityById(1)).thenReturn(user);
+        Assertions.assertInstanceOf(user.getClass(), userImpl.findEntityById(1));
+    }
+
+    @Test
+    void delete() {
+        Mockito.when(userImpl.delete(1)).thenReturn(true);
+        Assertions.assertTrue(userImpl.delete(1));
+    }
+
+    @Test
+    void update() {
+        Mockito.when(userImpl.update(new User(1,"Roman Druz", "+7(999)111-00-00" , "roman@mail.ru", 1))).thenReturn(new User());
+        Assertions.assertInstanceOf(User.class, userImpl.update(new User(1,"Roman Druz", "+7(999)111-00-00" , "roman@mail.ru", 1)));
+    }
+
 }
